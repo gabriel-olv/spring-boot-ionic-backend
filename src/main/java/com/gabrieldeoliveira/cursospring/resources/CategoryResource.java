03be367362f06c882d844d61c08d2420df0536f5
gabriel-olv <br.gabrieldeoliveira@gmail.com>
2023-03-04 13:25:35 -0300

    first domain class testing

--- src/main/java/com/gabrieldeoliveira/cursospring/resources/CategoryResource.java
+++ src/main/java/com/gabrieldeoliveira/cursospring/resources/CategoryResource.java
@@ -1,15 +1,22 @@
 package com.gabrieldeoliveira.cursospring.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gabrieldeoliveira.cursospring.domain.Category;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryResource {
 
     @GetMapping
-    public String list() {
-        return "REST works!";
+    public List<Category> list() {
+        Category c1 = new Category(1, "Informática");
+        Category c2 = new Category(2, "Escritório");
+        return Arrays.asList(c1, c2);
     }
 }
